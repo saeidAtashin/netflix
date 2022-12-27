631729bf0da8dc597f930b6142647b561e50cf4b
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2022-12-27 18:49:45 +0330

    start netflix project add firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD4yrItfmlYAJ7YqLxTFjkwnd69JcywCJY",
+    authDomain: "netflix2-b865d.firebaseapp.com",
+    projectId: "netflix2-b865d",
+    storageBucket: "netflix2-b865d.appspot.com",
+    messagingSenderId: "441387390853",
+    appId: "1:441387390853:web:74e48cfd5602d7103896ec"
+  };
