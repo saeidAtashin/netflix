863b08997ee5884ef70590dac94a85b771019b92
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2023-01-25 01:40:09 +0330

    firebase.js file error resolved

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
-// import firebase from 'firebase'
-// import { initializeApp } from 'firebase/app';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD4yrItfmlYAJ7YqLxTFjkwnd69JcywCJY",
-    authDomain: "netflix2-b865d.firebaseapp.com",
-    projectId: "netflix2-b865d",
-    storageBucket: "netflix2-b865d.appspot.com",
-    messagingSenderId: "441387390853",
-    appId: "1:441387390853:web:74e48cfd5602d7103896ec"
-  };
+  apiKey: "AIzaSyD4yrItfmlYAJ7YqLxTFjkwnd69JcywCJY",
+  authDomain: "netflix2-b865d.firebaseapp.com",
+  projectId: "netflix2-b865d",
+  storageBucket: "netflix2-b865d.appspot.com",
+  messagingSenderId: "441387390853",
+  appId: "1:441387390853:web:74e48cfd5602d7103896ec",
+};
 
+// Use this to initialize the firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-  // const firebaseApp = firebase.initializeApp(firebaseConfig);
-  // const db = firebaseApp.fireStore();
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
 
-
-  // const auth = firebase.auth();
-
-  // export { auth };
-  // export default db;
+export { auth, db };
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,27 +1,24 @@
-
 import React, { useRef } from "react";
 import { auth } from "../firebase";
 import "./SignInScreen.css";
 
 const SignInScreen = () => {
-
-const emailRef = useRef(null)
-const passwordRef = useRef(null)
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
   const register = (e) => {
     e.preventDefault();
     auth
-    .createUserWithEmailAndPassword(
-      emailRef.current.value,
-      passwordRef.current.value
-    )
-    .then((authUser) => {
-      console.log(authUser)
-    })
-    .catch((error) => {
-      alert(error.massage)
-    })
-
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.massage);
+      });
   };
 
   const signIn = (e) => {
@@ -31,20 +28,20 @@ const passwordRef = useRef(null)
   return (
     <div className="signInScreen">
       <h1>Sign In</h1>
-            <form className="form">
-              <div className="textbox">
-                <div className="material-symbols-outlined"> email </div>
-                <input type="text" className="input__buttom" required />
-                <label>Email</label>
-              </div>
-              <div className="textbox">
-                <div className="material-symbols-outlined"> key </div>
-                <input type="password" className="input__buttom" required />
-                <label>Password</label>
-              </div>
+      <form className="form">
+        <div className="textbox">
+          <div className="material-symbols-outlined"> email </div>
+          <input type="text" className="input__buttom" required />
+          <label>Email</label>
+        </div>
+        <div className="textbox">
+          <div className="material-symbols-outlined"> key </div>
+          <input type="password" className="input__buttom" required />
+          <label>Password</label>
+        </div>
         <p>
           New to Netflix?
-          <span onClick={signIn}>Sign Up now.</span>
+          <span onClick={register}>Sign Up now.</span>
         </p>
 
         <button type="submit" className="input__buttom" onClick={signIn}>
