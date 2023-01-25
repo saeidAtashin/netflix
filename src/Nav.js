33f449d5b200ef490fa64ff8fba120cc90b86bf8
Saeid Atashin <saeidsaeid@outlook.com>
2023-01-26 00:50:47 +0330

    profileScreen added

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 function Nav() {
 
 
     const [show, handleShow] = useState(false)
+    const navigate = useNavigate()
 
     const transitionNavbar = () => {
         if (window.scrollY > 100){
@@ -25,6 +27,7 @@ useEffect(() => {
     <div className={`nav ${show && 'nav__black'}`}>
       <div className="nav__contents">
         <img
+        onClick={() => navigate('/')}
           className="nav__logo"
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt="logo"
@@ -33,6 +36,7 @@ useEffect(() => {
           className="nav__avatar"
           src="https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png"
           alt="logo"
+          onClick={() => navigate('/profile')}
         />
       </div>
     </div>
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import './ProfileScreen.css'
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { auth } from "../firebase";
+import Nav from "../Nav";
+import "./ProfileScreen.css";
 
 function ProfileScreen() {
+  const user = useSelector(selectUser);
+
   return (
-    <div className='profileScreen'>
-      text
+    <div className="profileScreen">
+      <Nav />
+      <div className="profileScreen__body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector-PNG-File.png"
+            alt="logo"
+          />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              plans
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen__signOut"
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProfileScreen
+export default ProfileScreen;
